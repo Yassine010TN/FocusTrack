e9a734b7b2c0fdfc9705cd7e45f824cfd0ebf51a
Yassine Kechiche <y.kechiche@tu-braunschweig.de>
2025-03-11 21:39:59 +0100

    init app

---
+++ focustrack/src/main/java/com/focustrack/backend/FocustrackApplication.java
@@ -0,0 +1,13 @@
+package com.focustrack.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocustrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocustrackApplication.class, args);
+	}
+
+}
---
+++ focustrack/src/test/java/com/focustrack/backend/FocustrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.focustrack.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocustrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
