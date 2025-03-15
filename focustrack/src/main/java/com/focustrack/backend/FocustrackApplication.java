81f48d507dc05c2002c406881b2addd771ca13d7
Yassine Kechiche <y.kechiche@tu-braunschweig.de>
2025-03-15 16:43:47 +0100

    fix

--- focustrack/src/main/java/com/focustrack/backend/FocustrackApplication.java
+++ focustrack/src/main/java/com/focustrack/backend/FocustrackApplication.java
@@ -2,6 +2,8 @@ package com.focustrack.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class FocustrackApplication {
@@ -10,4 +12,8 @@ public class FocustrackApplication {
 		SpringApplication.run(FocustrackApplication.class, args);
 	}
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
