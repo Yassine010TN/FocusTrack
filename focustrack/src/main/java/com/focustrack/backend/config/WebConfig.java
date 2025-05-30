fb09af4262a9231ebade0103162d83043e0a4654
Yassine Kechiche <y.kechiche@tu-braunschweig.de>
2025-05-30 17:47:11 +0200

    update config

--- focustrack/src/main/java/com/focustrack/backend/config/WebConfig.java
+++ focustrack/src/main/java/com/focustrack/backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // frontend
+                .allowedOrigins("http://localhost:5173", "https://focus-track-fe.vercel.app") //
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
